ebf8113a00ce5a0f733d03833317e823e103172d
vezono <vezonolab@gmail.com>
2022-05-06 02:15:36 +0300

    new update

--- scripts/scripts/s6start.sh
+++ scripts/scripts/s6start.sh
@@ -1,5 +1,6 @@
 killall -q s6-svscan
 killall -q s6-supervise
+killall -q s6-log
 s6-svscanctl -t /run/user/1000/s6/scandir
 
 ramdir="/run/user/$(id -u)"
--- scripts/scripts/weather.sh
+++ scripts/scripts/weather.sh
@@ -1,2 +1,2 @@
-curl wttr.in/Dnipro?format=1 2> /dev/null
+curl wttr.in/Toronto?format=1 2> /dev/null
 #echo 1
