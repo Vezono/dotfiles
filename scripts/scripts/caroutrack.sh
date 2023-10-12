4ff36324842094206d9696021f0dc88e706c02e9
vezono <vezono.lab@gmail.com>
2023-10-11 22:35:13 -0400

    caroutrack optimize

--- scripts/scripts/caroutrack.sh
+++ scripts/scripts/caroutrack.sh
@@ -1,44 +1,46 @@
 #!/bin/bash
 
+# Constants and initializations
 HEADPHONES_EMOJI="󰋋"
 DISPLAY_LEN=40
-
-LEFT_CUT=0
-RIGHT_CUT=0
-
 REVERSE=0
-
 PREV_TRACK=""
+DISPLAY_TEXT=""
 
 get_status_emoji() {
+    # Check the player status using playerctl and return appropriate emoji
     if [ "$(playerctl status 2> /dev/null)" = "Playing" ]; then
-            echo "󰏤"
-        else
-            echo "󰐊"
+        echo "󰏤"
+    else
+        echo "󰐊"
     fi
 }
 
 get_track() {
-    CURRENT_TRACK=`~/scripts/track.sh`
+    # Get the current track using your track.sh script
+    CURRENT_TRACK=$(~/scripts/track.sh)
 }
 
 get_len() {
+    # Get the length of a string
     local text="$@"
     echo ${#text}
 }
 
+# Initial track retrieval
 get_track
 
-while true
-do
+while true; do
     get_track
-    len=$(get_len $CURRENT_TRACK)
+    len=$(get_len "$CURRENT_TRACK")
 
+    # Display track if it's shorter than the display length
     if [ "$len" -lt "$DISPLAY_LEN" ]; then
         echo "%{F#FDD835}$HEADPHONES_EMOJI%{F-} $CURRENT_TRACK %{F#FDD835}$(get_status_emoji)"
         continue
     fi
 
+    # Initialize cut positions if the track has changed
     if [ "$PREV_TRACK" != "$CURRENT_TRACK" ]; then
         LEFT_CUT=0
         RIGHT_CUT=$((len - DISPLAY_LEN))
@@ -48,6 +50,7 @@ do
     if [ "$REVERSE" -eq 0 ]; then
         ((LEFT_CUT++))
         ((RIGHT_CUT--))
+        # Switch to reverse scrolling if needed
         if [ "$LEFT_CUT" -ge $((len - DISPLAY_LEN)) ]; then
             REVERSE=1
         fi
@@ -59,13 +62,15 @@ do
         ((RIGHT_CUT++))
     fi
 
+    # Create the text to display
     DISPLAY_TEXT="${CURRENT_TRACK:${LEFT_CUT}}"
-    if [ "$RIGHT_CUT" -gt 0 ] && [ $(get_len $DISPLAY_TEXT) -gt $RIGHT_CUT ]; then
-       DISPLAY_TEXT="${DISPLAY_TEXT:0:-${RIGHT_CUT}}"
+    if [ "$RIGHT_CUT" -gt 0 ] && [ $(get_len "$DISPLAY_TEXT") -gt $RIGHT_CUT ]; then
+        DISPLAY_TEXT="${DISPLAY_TEXT:0:-${RIGHT_CUT}}"
     fi
 
+    # Print the formatted text
     echo "%{F#FDD835}$HEADPHONES_EMOJI%{F-} $DISPLAY_TEXT %{F#FDD835}$(get_status_emoji)"
+
+    # Sleep for a short duration
     sleep 0.1
 done
-
-
