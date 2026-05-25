0258d9465aecc23fcb6cf7f078a60dc4cc0f6b4f
Vezono <vezono@gts.org.ua>
2026-05-24 23:08:32 -0400

    minor updates

--- scripts/scripts/french_accents.sh
+++ scripts/scripts/french_accents.sh
@@ -57,4 +57,6 @@ EOF
 
 result=$(echo "$accents" | fuzzel -d | awk -F '- ' '{print $2}' | tr -d '\n')
 echo $result | wl-copy
-cb=$(wl-paste); wl-copy "$result" && ydotool key 29:1 42:1 47:1 47:0 42:0 29:0 && wl-copy "$cb"
+wl-copy "$result"
+# the line below pastes the selected symbol while saving the previous clipboard content. requires ydotool running.
+# cb=$(wl-paste); wl-copy "$result" && ydotool key 29:1 42:1 47:1 47:0 42:0 29:0 && wl-copy "$cb"
